acd3cb5e35188991b243e824c79091c33bf2da7a
FabianPitoy <fabianmpitoy@gmail.com>
2025-07-11 20:05:40 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 // pages/index.tsx
+//import HomePage from '@/components/HomePage';
+
+//export default function IndexPage() {
+ // return <HomePage />;
+//}
 import HomePage from '@/components/HomePage';
 
 export default function IndexPage() {
